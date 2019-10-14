406b457ac757e7ac8c39fe83e98cf8d6975f5c5d
Adriano Dijan <ldijan00@fesb.hr>
2019-10-14 22:54:47 +0200

    ispocetka

--- zd1.c
+++ zd1.c
@@ -1,43 +1,95 @@
-#ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STRMAX 1024
+
 typedef struct _student {
-	char ime[10];
-	char prezime[10];
+	char ime[STRMAX];
+	char prezime[STRMAX];
 	int bodovi;
-}Student;
+} Student;
+
+int prebrojiStudente(char * filename);
+int unosStudenata(char * filename, Student * studenti, int br_stud);
+int pronadiMax(Student * studenti, int br_stud);
+int ispisStudenata(Student * studenti, int br_stud, int maxbodovi);
 
 int main()
 {
-	FILE * fp;
-	int br_red = 0;
-	int i;
+	int br_stud = 0, maxbodovi = 0;
+	char filename[] = "studenti.txt";
 	Student * studenti;
-	int maxbr;
-	char buf[1024];
 
-	fp = fopen("C:\\Users\\student\\Documents\\studenti.txt", "r");
+	br_stud = prebrojiStudente(filename);
+	studenti = (Student *)malloc(br_stud*sizeof(Student));
 
-	while (fgets(buf, 60, fp)) br_red++;
-	studenti = (Student *)malloc(sizeof(Student) * br_red);
+	unosStudenata(filename, studenti, br_stud);
+	maxbodovi = pronadiMax(studenti, br_stud);
 
-	rewind(fp);
-
-	for (i = 0; i < br_red; i++) {
-		fscanf(fp, "%d %s %s", &(studenti[i].bodovi), studenti[i].ime, studenti[i].prezime);
-	}
-
-	maxbr = studenti[0].bodovi;
-	for(i=0; i<br_red; i++)
-		if (studenti[i].bodovi > maxbr) maxbr = studenti[i].bodovi;
-
-	for (i = 0; i < br_red; i++) printf("Ime: %s, Prezime: %s, Bodovi: %d, Relativni: %.2lf\n", studenti[i].ime, studenti[i].prezime, studenti[i].bodovi, (double)(studenti[i].bodovi*100/maxbr));
-
-	system("PAUSE");
+	ispisStudenata(studenti, br_stud, maxbodovi);
 
 	return 0;
+}
 
+int prebrojiStudente(char * filename)
+{
+	FILE * fp = NULL;
+	int br_stud = 0;
+	char dbuffer[STRMAX];
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+		return -1;
+
+	while (fgets(dbuffer, STRMAX, fp))
+		br_stud++;
+	
+	fclose(fp);
+
+	return br_stud;
+}
+
+int unosStudenata(char * filename, Student * studenti, int br_stud)
+{
+	FILE * fp = NULL;
+	int i = 0;
+
+	fp = fopen(filename, "r");
+
+	if(fp == NULL)
+		return -1;
+
+	for(i = 0; i<br_stud; i++)
+		fscanf(fp, "%d %s %s", &(studenti[i].bodovi), studenti[i].ime, studenti[i].prezime);
+
+	fclose(fp);
+
+	return 0;
+}
+
+int pronadiMax(Student * studenti, int br_stud)
+{
+	int i = 0;
+	int max = studenti[i].bodovi;
+
+	for(i=1; i<br_stud; i++) 
+		if (studenti[i].bodovi > max)
+			max = studenti[i].bodovi;
+
+	return max;
+}
+
+int ispisStudenata(Student * studenti, int br_stud, int maxbodovi)
+{
+	int i = 0;
+
+	for (i=0; i<br_stud; i++) {
+		double relativni = ((double)studenti[i].bodovi/maxbodovi)*100;
+		printf("Ime: %s\nPrezime: %s\nApsolutni broj bodova: %d\nRelativni broj bodova: %.3lf\n\n",\
+		studenti[i].ime, studenti[i].prezime, studenti[i].bodovi, relativni);
+	}
+
+	return 0;
 }
